f1ecbc3902be19b4860deaaaad811e563a809fff
Maddy Manya <manya.21535@sscbs.du.ac.in>
2024-02-04 20:29:10 +0530

    with changed parameters

---
+++ q3a_modified.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <graphics.h>
+#include <cmath>
+using namespace std;
+
+void ddaline(int x1, int y1, float m)
+{
+    float x = x1;
+    float y = y1;
+    for (int i = 0; i < 1000; ++i)
+    {
+        cout << "(" << round(x) << "," << round(y) << ")\n";
+        putpixel(round(x), round(y), 12);
+
+        if (m > 1)
+        {
+            putpixel(x, y, 12);
+            y += 1;
+            x = x + (1 / m);
+        }
+        else if (m <= 1)
+        {
+            putpixel(x, y, 12);
+            x += 1;
+            y = y + m;
+        }
+    }
+}
+
+int main()
+{
+
+    int x1, y1, m;
+    cout << "Enter values for (x1,y1) : ";
+    cin >> x1 >> y1;
+    cout << "Enter values for slope : ";
+    cin >> m;
+    cout << "Resulting Points Using DDA's Algorithm ->\n";
+
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char *)"");
+    ddaline(x1, y1, m);
+    getch();
+    return 0;
+}
